13f13da448a0433fef4a5843be3245002c238196
Vikash kumar <116914527+vksr8102@users.noreply.github.com>
2025-10-06 23:19:29 +0530

    Update docker_push.groovy

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -2,9 +2,9 @@ def call(Map config = [:]) {
     def imageName = config.imageName ?: error("Image name is required")
     def imageTag = config.imageTag ?: 'latest'
     def credentials = config.credentials ?: 'docker-hub-credentials'
-    
+
     echo "Pushing Docker image: ${imageName}:${imageTag}"
-    
+
     withCredentials([usernamePassword(
         credentialsId: credentials,
         usernameVariable: 'DOCKER_USERNAME',
@@ -12,8 +12,14 @@ def call(Map config = [:]) {
     )]) {
         sh """
             echo "\$DOCKER_PASSWORD" | docker login -u "\$DOCKER_USERNAME" --password-stdin
+
+            # Tag the local image with the remote name
+            docker tag notes-app:latest ${imageName}:${imageTag}
+
+            # Push to Docker Hub
             docker push ${imageName}:${imageTag}
-            docker push ${imageName}:latest
+
+            docker logout
         """
     }
 }
